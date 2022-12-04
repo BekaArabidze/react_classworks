31b1d2cc86c64779c323614b831a8bcc0c478133
Klaatu98 <arabson1998@gmail.com>
2022-12-04 13:46:27 +0400

    add how to use props

--- Lesson_1/src/App.tsx
+++ Lesson_1/src/App.tsx
@@ -1,40 +1,13 @@
-import { useState } from 'react'
 import "./style.css"
+import { useState } from 'react'
 import Counter from './components/Counter'
 import ColorFlipper from './components/ColorFlipper/ColorFlipper'
 import Reminder from './components/Reminder/Reminder'
 
 function App() {
-    // const [count, setCount] = useState(0)
-
-
-    // let string = "string"
-    // let number = 0
-    // let array = [1, 2, 3, 4]
-    // let obj = {
-    //     name: "Sam",
-    //     age: 20
-    // }
-
-    // function increment() {
-    //     setCount(count + 1)
-    // }
-
-
     return (
         <div className="App">
 
-            {/* <h1>{string}</h1>
-
-            <h1>{number}</h1>
-
-            <h1>{array.map((el, i) => (
-                <h1>{el}</h1>
-            ))}
-            </h1>
-
-            <p>{obj.name}</p>
-            <p>{obj.age}</p> */}
 
 
             {/* passing props to Counter component */}
@@ -43,8 +16,7 @@ function App() {
 
             {/* JUST IMPORT COMPONENET AND MOUNT HERE */}
             {/* <ColorFlipper /> */}
-            <Reminder/>
-
+            {/* <Reminder/> */}
         </div>
     )
 }
